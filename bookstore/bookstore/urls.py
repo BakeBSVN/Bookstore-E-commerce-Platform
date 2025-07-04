2e0824038c21ed2d51e47fa68ab356063f2c87c2
BakeBSVN <hphong1v1@gmail.com>
2025-07-04 10:13:00 +0700

    ubuntu

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', include('bookshop.urls')),
     path('orders/', include('orders.urls')),
     path('payment/', include('payment.urls')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
